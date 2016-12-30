0eaea75aaf5118c529b89b33c73486cf4a98a33c
hangilc <hangil@chang.jp>
2016-12-30 11:11:59 +0900

    added templates

--- out/main.js
+++ out/main.js
@@ -1,10 +1,7 @@
-define(["require", "exports", "text!template.html"], function (require, exports, tmpl) {
+define(["require", "exports"], function (require, exports) {
     "use strict";
     function run() {
-        var d = document.createElement("div");
-        var t = document.createTextNode(tmpl + "!");
-        d.appendChild(t);
-        document.getElementById("main").appendChild(d);
+        console.log("main started");
     }
     exports.run = run;
 });
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { msg } from './hello'
-import * as tmpl from "text!template.html"
 
 export function run(){
-	let d = document.createElement("div");
-	let t = document.createTextNode(tmpl + "!");
-	d.appendChild(t);
-	document.getElementById("main").appendChild(d);
+	console.log("main started");
 }
